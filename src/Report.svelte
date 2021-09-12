c1e0242955150a96d8f5462a8edf4e0ab49824b1
tanacchi <q111026d@mail.kyutech.jp>
2021-09-12 18:47:42 +0900

    Add other input form to Report page

--- src/Report.svelte
+++ src/Report.svelte
@@ -1,12 +1,24 @@
 <script>
-    import { onDestroy, text } from "svelte/internal";
+    import { each, onDestroy } from "svelte/internal";
     import TaskList from './TaskList.svelte';
     import { taskList } from './taskList';
 
     let date = "2020-09-08";
 
-    let newTask = "";
+    // Aim
+    let aims = ["ahi"];
+    let newAim = "";
 
+    const onNewAimEntered = (event) => {
+        if (event.keyCode != 13 || ! newAim) return;
+        aims = [...aims, newAim];
+        console.log(newAim);
+        newAim = "";
+    }
+
+
+    // Tasks
+    let newTask = "";
     let tasks;
     const unsubscribe = taskList.subscribe(val => {
         tasks = val;
@@ -15,17 +27,22 @@
     $: dones = tasks.filter(task => task.status == "done");
     onDestroy(unsubscribe);
 
-
     const onNewTaskEntered = (event) => {
         if (event.keyCode != 13 || ! newTask) return;
         taskList.addTask(newTask);
         newTask = "";
-        console.log("onNew");
     }
 
     const onTaskStatuToggled = (event) => {
         taskList.toggleStatus(event.detail.id);
     }
+
+
+    // Learned
+    let learned = "";
+
+    // Impressions
+    let impressions = "";
 </script>
 
 <p>
@@ -34,6 +51,14 @@
 
 <div>
     <h2>目標</h2>
+    <input type="text"
+           bind:value={newAim}
+           on:keyup={onNewAimEntered}>
+    <p>
+        {#each aims as aim}
+            <li>{aim}</li>
+        {/each}
+    </p>
     <h2>やること</h2>
     <input type="text"
            placeholder="やること追加"
@@ -45,7 +70,9 @@
     <h2>できなかったこと</h2>
     <TaskList tasks={undones} on:toggle={onTaskStatuToggled} />
     <h2>今日の学び</h2>
+    <textarea cols="30" rows="5" bind:value={learned}></textarea>
     <h2>感想</h2>
+    <textarea cols="30" rows="5" bind:value={impressions}></textarea>
 </div>
 
 
